99a762d7db252fe66bc89b86093407cf3354f686
Alexandra Miller <alexandramiller@Alexandras-MacBook-Pro.local>
2019-05-07 14:30:28 -0500

    completed practice prompt

--- practice/practice.js
+++ practice/practice.js
@@ -1,6 +1,11 @@
 // What do you think this first line does? 
 document.addEventListener('DOMContentLoaded', function() {
 	// your code goes below
-	
+	console.log("the page has loaded!");
+	var givenNumber = prompt("Give me a number");
+	console.log(givenNumber);
+	console.log("==", givenNumber==5);
+	console.log("===", givenNumber===5);
+	console.log("===string", givenNumber==="5");
 
 })
